4ca0b57b3bcbc7eb0890bbbba8e80dadac177ba9
Alexander Polus <alex.polus13@gmail.com>
2019-02-28 13:31:42 -0800

    Lab 8 done

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,13 @@
 function initMap() {
 	// add your code here
+	L.mapquest.key = 'nw2sWaDll9iBXYXmvnJ1USHfrS7dDCeS';
+
+	var map = L.mapquest.map('map', {
+		center: [32.8986895, -117.2359121],
+		layers: L.mapquest.tileLayer('map'),
+		zoom: 12,
+		zoomControl: false
+	});
+
+	L.marker([32.8986895, -117.2359121]).addTo(map);
 }
